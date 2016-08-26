4d768258c221b81b86d4abcee93f07b3ce329da3
anlegongli <anlegongli@gmail.com>
2016-08-26 00:10:03 -0700

    Add files via upload

---
+++ CSVParser/CSVReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSVParser
+{
+    public class CSVReader
+    {
+        public static List<Employee> getEmployees(string sPath, out bool isInvalid)
+        {
+            List<Employee> employees = new List<Employee>();
+            StreamReader reader = new StreamReader(sPath);
+
+            string record = reader.ReadLine();
+
+            record = reader.ReadLine();
+
+            isInvalid = false;
+
+            string employeeName;
+            decimal salary;
+            DateTime birthday;
+            DateTime dateHired;
+
+            while (!string.IsNullOrEmpty(record) && !isInvalid)
+            {
+                string[] sTempData = record.Split(',');
+
+                if (string.IsNullOrEmpty(sTempData[0]) || string.IsNullOrEmpty(sTempData[1]) || string.IsNullOrEmpty(sTempData[2]) || string.IsNullOrEmpty(sTempData[3]))
+                {
+                    isInvalid = true;
+                }
+
+                if (!isInvalid)
+                {
+                    try
+                    {
+                        salary = decimal.Parse(sTempData[2]);                                               
+                        employeeName = sTempData[0].Replace("\"", "");                         
+                        birthday = DateTime.Parse(sTempData[1]);
+                        dateHired = DateTime.Parse(sTempData[3]);
+
+                        if (salary > 0)
+                        {
+                            Employee employee = new Employee(employeeName, birthday, salary, dateHired);
+                            employees.Add(employee);
+                        }
+                        else
+                        {
+                            isInvalid = true;
+                        }
+                        
+                    }
+                    catch (Exception)
+                    {
+                        isInvalid = true;
+                    }
+
+                }
+                record = reader.ReadLine();
+
+            }
+                return employees;
+        }
+    }
+}
---
+++ CSVParser/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSVParser
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+        public DateTime Birthday { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateHired { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string employename, DateTime birthday, decimal salary, DateTime datehired)
+        {
+            this.EmployeeName = employename;
+            this.Birthday = birthday;
+            this.Salary = salary;
+            this.DateHired = datehired;
+        }
+    }
+}
